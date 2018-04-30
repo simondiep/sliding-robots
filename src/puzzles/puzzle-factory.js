a5fc0021e86471672bff6a5fb8e83f1e18644e9a
Simon Diep <simondiep1@gmail.com>
2018-04-30 17:02:59 -0500

    Cleaned up puzzle-factory

--- src/puzzles/puzzle-factory.js
+++ src/puzzles/puzzle-factory.js
@@ -2,48 +2,44 @@ import Coordinate from '../model/coordinate.js';
 import Robot from '../model/robot.js';
 import { getBlueRobotImage, getYellowRobotImage } from '../view/dom-helper.js';
 
-export function getRandomPuzzleId() {
-  return puzzleGetters[Math.floor(Math.random() * puzzleGetters.length)]().id;
-}
+const puzzleFactory = {
+  1: () => ({
+    id: 1,
+    board: {
+      SQUARE_SIZE_IN_PIXELS: 50,
+      HORIZONTAL_SQUARES: 20,
+      VERTICAL_SQUARES: 15,
+    },
+    robots: [
+      new Robot(new Coordinate(1, 1), 'yellow', getYellowRobotImage()),
+      new Robot(new Coordinate(2, 2), 'blue', getBlueRobotImage()),
+    ],
+    goalLocation: new Coordinate(3, 3),
+    walls: [new Coordinate(5, 2)],
+    minimumNumberOfMoves: 4,
+  }),
+  2: () => ({
+    id: 2,
+    board: {
+      SQUARE_SIZE_IN_PIXELS: 50,
+      HORIZONTAL_SQUARES: 20,
+      VERTICAL_SQUARES: 15,
+    },
+    robots: [
+      new Robot(new Coordinate(10, 10), 'yellow', getYellowRobotImage()),
+      new Robot(new Coordinate(5, 5), 'blue', getBlueRobotImage()),
+    ],
+    goalLocation: new Coordinate(2, 2),
+    walls: [new Coordinate(2, 8), new Coordinate(11, 10)],
+    minimumNumberOfMoves: 5,
+  }),
+};
 
-const puzzleGetters = [];
+export function getRandomPuzzleId() {
+  const ids = Object.keys(puzzleFactory);
+  return ids[Math.floor(Math.random() * ids.length)];
+}
 
 export function getPuzzle(puzzleId) {
-  return puzzleGetters.find(function(puzzleGetter) {
-    return puzzleGetter().id === puzzleId;
-  })();
+  return puzzleFactory[puzzleId]();
 }
-
-const getPuzzle1 = () => ({
-  id: 1,
-  board: {
-    SQUARE_SIZE_IN_PIXELS: 50,
-    HORIZONTAL_SQUARES: 20,
-    VERTICAL_SQUARES: 15,
-  },
-  robots: [
-    new Robot(new Coordinate(1, 1), 'yellow', getYellowRobotImage()),
-    new Robot(new Coordinate(2, 2), 'blue', getBlueRobotImage()),
-  ],
-  goalLocation: new Coordinate(3, 3),
-  walls: [new Coordinate(5, 2)],
-  minimumNumberOfMoves: 4,
-});
-puzzleGetters.push(getPuzzle1);
-
-const getPuzzle2 = () => ({
-  id: 2,
-  board: {
-    SQUARE_SIZE_IN_PIXELS: 50,
-    HORIZONTAL_SQUARES: 20,
-    VERTICAL_SQUARES: 15,
-  },
-  robots: [
-    new Robot(new Coordinate(10, 10), 'yellow', getYellowRobotImage()),
-    new Robot(new Coordinate(5, 5), 'blue', getBlueRobotImage()),
-  ],
-  goalLocation: new Coordinate(2, 2),
-  walls: [new Coordinate(2, 8), new Coordinate(11, 10)],
-  minimumNumberOfMoves: 5,
-});
-puzzleGetters.push(getPuzzle2);
